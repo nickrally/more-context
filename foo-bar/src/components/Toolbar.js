2f6503e4d8d0ae85b904878ef5c8c9d7c863c50b
nickrally <nickrally2009@gmail.com>
2021-09-24 16:29:47 -0600

    exporting vars from scss and setting vars values in scss from component

--- foo-bar/src/components/Toolbar.js
+++ foo-bar/src/components/Toolbar.js
@@ -1,18 +1,21 @@
 import "./Toolbar.scss";
 import AppContext from "./AppContext";
-import variables from "./styles/color-variables.scss";
+import colors from "./styles/color-variables.module.scss";
 import { useContext, useRef, useEffect } from "react";
 
 const Toolbar = (props) => {
-  console.log(variables);
   const toolbarRef = useRef();
   const appCtx = useContext(AppContext);
   //toolbarRef.current.style.setProperty("--backcolor", "red");
   //TypeError: Cannot read properties of undefined (reading 'style')
+  //setProperty in useEffect, after component is rendered
   const colorClassName = "someborder backcolor-red";
   useEffect(() => {
-    toolbarRef.current.style.setProperty("--backcolor", variables.lightred);
-  }, []);
+    toolbarRef.current.style.setProperty(
+      "--backcolor",
+      colors["light" + appCtx.color.toLowerCase()]
+    );
+  }, [appCtx.color]);
   return (
     <div className={colorClassName} ref={toolbarRef}>
       {props.children}
